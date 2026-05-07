f555fba98381941fa5adb73b8c6ee967ca9d552f
crsanches <crsanches4@gmail.com>
2026-05-07 14:53:20 -0300

    jogos teste mocado 1

--- data/jogos.ts
+++ data/jogos.ts
@@ -1,34 +1,27 @@
 import { Match } from "../types/match";
 
 export const jogos: Match[] = [
+
   {
-    phase: "Grupos",
     teamA: "Brasil",
     teamB: "Argentina",
     emojiA: "🇧🇷",
     emojiB: "🇦🇷",
-    resultadoA: 2,
-    resultadoB: 1,
-    matchDate: "2026-05-07T02:00:00"
-  },
-  {
-    phase: "Oitavas de final",
-    teamA: "França",
-    teamB: "Espanha",
-    emojiA: "🇫🇷",
-    emojiB: "🇪🇸",
     resultadoA: 0,
     resultadoB: 0,
-    matchDate: "2026-05-07T02:00:00"
+    phase: "Amistosos",
+    matchDate: "2026-05-07T19:00:00"
   },
+
   {
-    phase: "Quartas de final",
-    teamA: "Alemanha",
-    teamB: "Itália",
-    emojiA: "🇩🇪",
-    emojiB: "🇮🇹",
-    resultadoA: 1,
-    resultadoB: 3,
-    matchDate: "2026-05-07T02:00:00"
+    teamA: "Holanda",
+    teamB: "Alemanha",
+    emojiA: "🇳🇱",
+    emojiB: "🇩🇪",
+    resultadoA: 0,
+    resultadoB: 0,
+    phase: "Amistosos",
+    matchDate: "2026-05-07T19:00:00"
   }
+
 ];
