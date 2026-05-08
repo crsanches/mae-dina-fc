a05a9e09282a4b5226a470e8a72f5c4257c364b0
crsanches <crsanches4@gmail.com>
2026-05-07 21:32:37 -0300

    iniciando firebase

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,14 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_AUTH_DOMAIN",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_STORAGE_BUCKET",
-  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
-  appId: "SEU_APP_ID"
-};
+    apiKey: "AIzaSyDG-I0M_P9qtINf9kmceaeNOrOMSx7XIQg",
+    authDomain: "mae-dina-fc.firebaseapp.com",
+    projectId: "mae-dina-fc",
+    storageBucket: "mae-dina-fc.firebasestorage.app",
+    messagingSenderId: "204520739342",
+    appId: "1:204520739342:web:d41610e7796cd0ed1ea25f"
+  };
 
 const app = initializeApp(firebaseConfig);
 
