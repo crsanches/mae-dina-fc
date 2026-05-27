a2b5fef9e95f9d186e46eb8f7b62176161a5c142
crsanches <crsanches4@gmail.com>
2026-05-26 22:38:13 -0300

    logo casa da mae joana2

--- app/page.tsx
+++ app/page.tsx
@@ -691,7 +691,7 @@ export default function Home() {
                 src="/logos/casa_mae_joana.png"
                 alt="Auditoria"
                 className="
-                  w-88
+                  w-58
                   object-contain
                   drop-shadow-2xl
                 "
