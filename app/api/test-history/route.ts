d0ce0face208c1530ef24d3e415369eb80637478
crsanches <crsanches4@gmail.com>
2026-08-08 00:59:04 -0300

    incluir libertadores e acertar visao jogos ida e volta3

--- app/api/test-history/route.ts
+++ app/api/test-history/route.ts
@@ -1,13 +1,14 @@
 import { NextResponse } from "next/server";
-
-import {
-  buildLeagueHistory
-} from "@/lib/buildLeagueHistory";
+import { buildLeagueHistory } from "@/lib/buildLeagueHistory";
+import { getTorneioAtivoAdmin } from "@/lib/getTorneioAtivoAdmin";
 
 export async function GET() {
 
+  const torneioId = await getTorneioAtivoAdmin();
+
   await buildLeagueHistory(
-    "S45ZeKwxkKSeYTqq2p45"
+    "S45ZeKwxkKSeYTqq2p45",
+    torneioId
   );
 
   return NextResponse.json({
